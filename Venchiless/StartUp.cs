623f648fae483a96670fa3bc5685b6b00fa9198f
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-27 15:33:46 +0300

    <All checks are ready.Only logic code must be remain to be written>

--- Venchiless/StartUp.cs
+++ Venchiless/StartUp.cs
@@ -18,12 +18,60 @@ namespace Vehicles
             double truckFuelQuantity = double.Parse(truckInfo[1]);
             double truckLitersPerKilometer = double.Parse(truckInfo[2]);
 
+            Car car = new Car(carFuelQuantity, carLitersPerKilometer);
+            Truck truck = new Truck(truckFuelQuantity, truckLitersPerKilometer);
 
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
-                string command = Console.ReadLine();
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = command[0];
+                string vehicle = command[1];
+                double value = double.Parse(command[2]);
+
+                if (action == "Drive")
+                {
+                    if (vehicle == "Car")
+                    {
+                        if (car.CanDrive(value))
+                        {
+                            car.Drive(value);
+                            Console.WriteLine($"Car travelled {value} km");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Car needs refueling");
+                        }
+                    }
+                    else
+                    {
+                        if (truck.CanDrive(value))
+                        {
+                            truck.Drive(value);
+                            Console.WriteLine($"Truck travelled {value} km");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Truck needs refueling");
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (vehicle == "Car")
+                    {
+                        car.Refuel(value);
+                    }
+                    else
+                    {
+                        truck.Refuel(value);
+                    }
+                }
+
             }
         }
     }
