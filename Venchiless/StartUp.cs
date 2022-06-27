6ea4134248fa1690931a2d55aa58d1933a9ebc5a
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-27 15:20:55 +0300

    <Classes and methods are already written and tested.>

--- Venchiless/StartUp.cs
+++ Venchiless/StartUp.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace Venchiless
+namespace Vehicles
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] carInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] truckInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double carFuelQuantity = double.Parse(carInfo[1]);
+            double carLitersPerKilometer = double.Parse(carInfo[2]);
+
+            double truckFuelQuantity = double.Parse(truckInfo[1]);
+            double truckLitersPerKilometer = double.Parse(truckInfo[2]);
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+            }
         }
     }
 }
---
+++ Venchiless/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        protected Vehicle(double fuelQuantity, double fuelConsumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public double FuelQuantity { get; set; }
+
+        public virtual double FuelConsumption { get; set; }
+
+        public bool CanDrive(double kilometers)
+            => this.FuelQuantity - (kilometers * this.FuelConsumption) >= 0;
+
+
+        public void Drive(double kilometers)
+        {
+            if (this.FuelQuantity - (kilometers * this.FuelConsumption) >= 0)
+            {
+                this.FuelQuantity -= kilometers * this.FuelQuantity;
+            }
+        }
+
+        public virtual void Refuel(double amount)
+        {
+            this.FuelQuantity += amount;
+        }
+
+
+    }
+}
