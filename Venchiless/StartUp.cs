2b156e20f5932cc41f59dc3677385d21a616b0fd
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-27 14:53:38 +0300

    <Starting project>

--- Venchiless/StartUp.cs
+++ Venchiless/StartUp.cs
@@ -2,9 +2,9 @@
 
 namespace Venchiless
 {
-    class Program
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
